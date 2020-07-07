9dfe77ad2f84b6d27ed3924f01873023508f69e0
Vaishnav Potlapalli <51985757+va1shn9v@users.noreply.github.com>
2020-07-07 19:39:09 +0530

    Update cascade_mask_rcnn_hrnetv2p_w32_20e.py

--- configs/cascade_mask_rcnn_hrnetv2p_w32_20e.py
+++ configs/cascade_mask_rcnn_hrnetv2p_w32_20e.py
@@ -202,7 +202,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=(512, 512), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+#     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
 ]

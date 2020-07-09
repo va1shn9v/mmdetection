8c122009e071d11e421287f95a36e66cd7e2d397
Vaishnav Potlapalli <51985757+va1shn9v@users.noreply.github.com>
2020-07-09 12:33:43 +0530

    Update cascade_mask_rcnn_hrnetv2p_w32_20e.py

--- configs/cascade_mask_rcnn_hrnetv2p_w32_20e.py
+++ configs/cascade_mask_rcnn_hrnetv2p_w32_20e.py
@@ -210,7 +210,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        img_scale=(512, 512),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
